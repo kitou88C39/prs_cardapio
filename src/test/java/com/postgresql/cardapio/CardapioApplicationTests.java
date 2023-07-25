348d9fedebc0169383aaffc5a629286f908782d7
kitou88C39 <kanfutrooper@icloud.com>
2023-07-25 17:33:20 +0900

    first commit

---
+++ src/test/java/com/postgresql/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgresql.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
