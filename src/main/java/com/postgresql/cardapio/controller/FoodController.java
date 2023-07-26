e4c68cdd2be35dbd70e184003b2724fb07380ddc
kitou88C39 <kanfutrooper@icloud.com>
2023-07-26 17:20:34 +0900

    controller/FoodController.javaを編集

--- src/main/java/com/postgresql/cardapio/controller/FoodController.java
+++ src/main/java/com/postgresql/cardapio/controller/FoodController.java
@@ -1,10 +1,11 @@
 package com.postgresql.cardapio.controller;
 
 import com.postgresql.cardapio.food.Food;
-import com.postgresql.cardapio.food.FoodReponseDTO;
 import com.postgresql.cardapio.food.FoodRepository;
+import com.postgresql.cardapio.food.FoodReponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -17,8 +18,14 @@ public class FoodController {
     private FoodRepository repository;
 
     @GetMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+
+    }
+
+    @GetMapping
     public List<FoodResponseDTO> getAll() {
-        List<Food> foodList = repository.findAll().stream().map(FoodReponseDTO::new);
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodReponseDTO::new);
         return foodList;
     }
 }
