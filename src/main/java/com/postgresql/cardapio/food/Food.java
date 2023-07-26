eff7061cf28d60e8053539eb52ba90e38f93e679
kitou88C39 <kanfutrooper@icloud.com>
2023-07-26 16:45:46 +0900

    food/Food.java を編集

--- src/main/java/com/postgresql/cardapio/food/Food.java
+++ src/main/java/com/postgresql/cardapio/food/Food.java
@@ -1,12 +1,21 @@
 package com.postgresql.cardapio.food;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 
-@Table(name="foods")
-@Entity(name="foods")
 public class Food {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long title;
