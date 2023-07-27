53ec42b9333305b900c5bd7424bc4c59836bf81a
kitou88C39 <kanfutrooper@icloud.com>
2023-07-27 10:23:19 +0900

    controller/FoodController.javaを編集

--- src/main/java/com/postgresql/cardapio/controller/FoodController.java
+++ src/main/java/com/postgresql/cardapio/controller/FoodController.java
@@ -5,6 +5,7 @@ import com.postgresql.cardapio.food.FoodRepository;
 import com.postgresql.cardapio.food.FoodResponseDTO;
 import com.postgresql.cardapio.food.FoodRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public void saveFood(@RequestBody FoodRequestDTO data) {
         Food foodData = new Food(data);
@@ -25,6 +27,7 @@ public class FoodController {
         return;
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll() {
 
